9809a2bd9f00f7710f3400986c608aaed62f1d50
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2020-08-29 02:18:44 -0300

    Se incluyo mapa

---
+++ public/javascripts/dist/map.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var mymap = L.map('mapid').setView([-32.9413724, -60.675548], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
---
+++ public/javascripts/map.js
@@ -0,0 +1,5 @@
+var mymap = L.map('mapid').setView([-32.9413724,-60.675548], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
